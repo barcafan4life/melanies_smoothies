aff22019f7bcecb31995fc361d8aa4c10eeb9239
barcafan4life <roque.rodriguez@mfsg.com>
2025-06-11 19:48:56 -0700

    Update streamlit_app.py
    add json data load to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ from snowflake.snowpark.functions import col
 #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
 
